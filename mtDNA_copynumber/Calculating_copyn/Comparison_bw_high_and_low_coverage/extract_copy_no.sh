9455fc395e7dd677ea4c2b522ca9edac70d51059
Edmundo Torres <edmundotogo@gmail.com>
2018-11-13 12:36:11 -0500

    Update mtDNA_copynumber/Calculating_copyn/Comparison_bw_high_and_low_coverage/extract_copy_no.sh
    Added a third argument that distinguishes high and low coverage alignments.
    The coverage distinction is then also added to the output files.
    
    Deleted lines specific to developer's computer or cluster.

--- mtDNA_copynumber/Calculating_copyn/Comparison_bw_high_and_low_coverage/extract_copy_no.sh
+++ mtDNA_copynumber/Calculating_copyn/Comparison_bw_high_and_low_coverage/extract_copy_no.sh
@@ -1,36 +1,23 @@
 #!/bin/bash
 
-#SBATCH -C new
-##SBATCH --cpus-per-task=8
-#SBATCH -J DNAdamage
-#SBATCH -t 14-00:00:00
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=saz5078@psu.edu
-#SBATCH -e slurm-%j.err
-#SBATCH -D /nfs/brubeck.bx.psu.edu/scratch3/arslan/mtnuc_project/copy_number
-
-
-source /nfs/brubeck.bx.psu.edu/scratch4/arslan/.bash_profile
-source activate cenv
-
 location=ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data
 iid=${1}
 bam=${2}
+coverage=${3}
+# low coverage = "alignment", high coverage = "high_coverage_alignment"
 
-alignment=${location}/${iid}/alignment/${bam}
-index=${location}/${iid}/alignment/${bam}.bai
+alignment=${location}/${iid}/${coverage}/${bam}
+index=${location}/${iid}/${coverage}/${bam}.bai
 
 read_length=`samtools view ${alignment} |head -n 1000 |awk '{sum+=length($10)} END{print sum/NR}'`
 echo $read_length
 echo "running idxstats"
 
-cd /nfs/brubeck.bx.psu.edu/scratch3/arslan/mtnuc_project/copy_number
-
-samtools idxstats ${alignment} > ${iid}.idxstats
+samtools idxstats ${alignment} > ${coverage}/idxstats/${iid}.idxstats
 
 echo "calculating depth of sequencing"
 
-Rscript cal_mtdna_copy.R ${iid}.idxstats ${read_length}
+Rscript cal_mtdna_copy.R ${coverage}/idxstats/${iid}.idxstats ${read_length}
 
 
 
